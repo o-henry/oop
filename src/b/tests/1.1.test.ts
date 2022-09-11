eff1a7d95d27ef6d1715c9c2c030031fa007b3ce
henry <cksgod16@gmail.com>
2022-09-11 19:01:40 +0900

    update

--- src/b/tests/1.1.test.ts
+++
@@ -1,9 +0,0 @@
-import { pipe } from "fp-ts/lib/function";
-
-describe("", () => {
-    it("", () => {
-        const double = (n: number): number => n * 2;
-
-        expect(pipe(2, double, double)).toBe(16);
-    });
-});
